fad422dc6e2395c4d8d1bed2bd3b63c2ed6a5ca1
YutaMakidera <99152013+YutaMakidera@users.noreply.github.com>
2022-02-08 17:47:39 +0900

    add orders

---
+++ Model/OrerModel.php
@@ -0,0 +1,52 @@
+<?php
+class OrderModel
+{
+  protected $pdo;
+  const USER = 'root';
+  const PASS = 'password';
+  const HOST = 'localhost';
+  const NAME = 'outdoor';
+  const DSN = 'mysql:host=localhost;dbname=outdoor;charset=utf8';
+
+  public function __construct()
+  {
+    $this->db_connect();
+  }
+  // DBに接続
+  public function db_connect()
+  {
+    try {
+      $pdo = new PDO($this::DSN, $this::USER, $this::PASS);
+      $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    } catch (PDOException $Exception) {
+      die('接続エラー：' . $Exception->getMessage());
+    }
+    return $pdo;
+  }
+
+  // 注文情報入力画面の表示
+  public function input()
+  {
+  }
+
+  // 注文情報確認画面の表示
+  public function check()
+  {
+  }
+
+  // 注文完了画面の表示
+  public function complete()
+  {
+  }
+
+  // 注文履歴一覧画面の表示
+  public function index()
+  {
+  }
+
+  // 注文履歴詳細画面の表示
+  public function show()
+  {
+  }
+
+}
