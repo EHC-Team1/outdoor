60dbf965325b839a4a2ea61d7edc66bae4c4d0e1
EguchiAkira0908 <Akira.Eguchi2@kusurinomadoguchi.co.jp>
2022-02-08 17:40:54 +0900

    Fix routing

---
+++ Model/AdminModel.php
@@ -0,0 +1,36 @@
+<?php
+class AdminModel
+{
+  protected $pdo;
+  const USER = 'root';
+  const PASS = 'password';
+  const HOST = 'localhost';
+  const NAME = 'outdoor';
+  const DSN = 'mysql:host=localhost;dbname=outdoor;charset=utf8';
+
+  public function __construct()
+  {
+    $this->db_connect();
+  }
+  // DBに接続
+  public function db_connect()
+  {
+    try {
+      $pdo = new PDO($this::DSN, $this::USER, $this::PASS);
+      $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    } catch (PDOException $Exception) {
+      die('接続エラー：' . $Exception->getMessage());
+    }
+    return $pdo;
+  }
+
+  // ログイン処理
+  public function login()
+  {
+  }
+
+  // ログアウト処理
+  public function logout()
+  {
+  }
+}
--- view_common/header.php
+++ view_common/header.php
@@ -3,8 +3,8 @@
 
 <head>
   <meta charset="UTF-8">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/bootstrap.css">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src='https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.5.0.js'></script>
