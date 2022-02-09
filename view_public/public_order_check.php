f729437c7aa96447c80d23919b97960ac51521e4
YutaMakidera <99152013+YutaMakidera@users.noreply.github.com>
2022-02-09 17:47:14 +0900

    注文関連の画面作成

--- view_public/public_order_check.php
+++ view_public/public_order_check.php
@@ -10,45 +10,73 @@ $pdo = new OrderModel();
 <?php require_once '../view_common/header.php'; ?>
 
 <div class="container">
-  <h1>注文情報の確認</h1>
+  <div class="d-flex align-items-center justify-content-center">
+    <h1 class="text-center mt-5 mb-5">注文情報の確認</h1>
+    <!-- 戻るボタンは仮置き -->
+    <button onclick="history.back();" class="btn btn-outline-secondary">戻る</button>
+  </div>
   <div class="row">
+    <div class="col-md-9">
+      <table border="1">
+        <tr>
+          <th>商品名</th>
+          <th>単価(税込み)</th>
+          <th>数量</th>
+          <th>小計</th>
+        </tr>
+        <tr>
+          <!-- 購入商品の表示 -->
+          <?php ?>
+          <td><? ?></td>
+          <td><? ?></td>
+          <td><? ?></td>
+          <td><? ?></td>
+          <?php ?>
+        </tr>
+      </table>
+    </div>
+    <div class="col-md-3">
+      <table border="1">
+        <tbody>
+          <tr>
+            <th>送料</th>
+            <td></td>
+          </tr>
+          <tr>
+            <th>商品合計</th>
+            <td></td>
+          </tr>
+          <tr>
+            <th>請求額</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-10">
+      <table>
+        <tbody>
+          <tr>
+            <th>支払方法</th>
+            <td><?php echo(htmlspecialchars($_POST['payment-way'], ENT_QUOTES)); ?></td>
+          </tr>
+          <tr>
+            <th>お届け先</th>
+            <td>〒<? ?> <? ?> <br><? ?></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div class="text-center mb-5">
     <form action="public_order_complete.php" method="POST">
-      <div class="form-group">
-        <table border="1">
-          <tr>
-            <th>商品名</th>
-            <th>単価(税込み)</th>
-            <th>数量</th>
-            <th>小計</th>
-          </tr>
-          <tr>
-            <!-- 購入商品の表示 -->
-            <?php ?>
-              <td><? ?></td>
-              <td><? ?></td>
-              <td><? ?></td>
-              <td><? ?></td>
-            <?php ?>
-          </tr>
-        </table>
-        <table border="1">
-          <tbody>
-            <tr>
-              <th>送料</th>
-              <td></td>
-            </tr>
-            <tr>
-              <th>商品合計</th>
-              <td></td>
-            </tr>
-            <tr>
-              <th>請求額</th>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <input type="submit" name="check_order" class="btn btn-outline-primary btn-lg" value="注文を確定する">
     </form>
   </div>
+</div>
+</div>
+
 </div>
 
 
--- view_public/public_order_input.php
+++ view_public/public_order_input.php
@@ -5,59 +5,76 @@ session_start();
 require_once('../Model/OrderModel.php');
 $pdo = new OrderModel();
 
+// Customerクラス呼び出し
+require_once('../Model/CustomerModel.php');
+$pdo = new CustomerModel();
+
+// 確認ボタンが押された場合
+if (isset($_POST['input_order'])) {
+  // Orderクラスを呼び出し
+  require_once('../Model/OrderModel.php');
+  $pdo = new OrderModel();
+  // inputクラスを呼び出し
+  $pdo = $pdo->input();
+  // エラーメッセージを$messageに格納
+  $message = $pdo;
+}
+$message = "";
+
 ?>
 
 <?php require_once '../view_common/header.php'; ?>
 
 <div class="container">
-  <div class="row">
-    <h1 class="">注文情報入力</h1>
-    <form action="public_order_check.php" method="POST">
-      <div class="form-group">
-        <label>支払方法</label>
-        <ul>
-          <li>
-            <input type="radio" id="transfer" name="payment-way">
-            <label for="transfer">振込み</label>
-          </li>
-          <li>
-            <input type="radio" id="card" name="payment-way">
-            <label for="card">クレジットカード</label>
-          </li>
-        </ul>
-        <label>お届け先</label>
-        <ul>
-          <li>
-            <input type="radio" id="my-address" name="delivery-target">
-            <label for="my-address">ご自身の住所</label><br>
-            <!-- ログインユーザーの住所を取得 -->
-            <?php ?>
-          </li>
-          <li>
-            <input type="radio" id="registration-address" name="delivery-target">
-            <label for="registration-address">登録先住所から選択</label><br>
-            <select name="address" id="address-select">
-              <option selected>
-                <!-- ユーザーが登録している住所を取得 -->
-                <?php  ?>
-              </option>
-            </select>
-          </li>
-          <li>
-            <input type="radio" id="new-address" name="delivery-target">
-            <label for="new-address">新しいお届け先</label><br>
-            <label for="">郵便番号(ハイフンなし)</label>
-            <input type="text" placeholder="0001111"><br>
-            <label for="">住所</label>
-            <input type="text" placeholder="東京都豊島区池袋0-0-0"><br>
-            <label for="">宛名</label>
-            <input type="text" placeholder="藤浪翔平"><br>
-          </li>
-        </ul>
-        <input type="submit" class="btn btn-outline-primary btn-lg" value="確認画面へ">
-      </div>
-    </form>
-
+  <div class="row d-flex align-items-center justify-content-center">
+    <h1 class="text-center mt-5 mb-5">注文情報入力</h1>
+    <div class="col-md-10">
+      <form action="public_order_check.php" method="POST">
+        <div class="form-group">
+          <label>支払方法</label>
+          <ul>
+            <li>
+              <label><input type="radio" id="transfer" name="payment-way" value="振込み"></label>
+              <label for="transfer">振込み</label>
+            </li>
+            <li>
+              <label><input type="radio" id="card" name="payment-way" value="クレジットカード"></label>
+              <label for="card">クレジットカード</label>
+            </li>
+          </ul>
+          <label>お届け先</label>
+          <ul>
+            <li>
+              <input type="radio" id="my-address" name="delivery-target">
+              <label for="my-address">ご自身の住所</label><br>
+              <!-- ログインユーザーの住所を取得 -->
+              <?php ?>
+            </li>
+            <li>
+              <input type="radio" id="registration-address" name="delivery-target">
+              <label for="registration-address">登録先住所から選択</label><br>
+              <select name="address" id="address-select">
+                <option selected>
+                  <!-- ユーザーが登録している住所を取得 -->
+                  <?php  ?>
+                </option>
+              </select>
+            </li>
+            <li>
+              <input type="radio" id="new-address" name="delivery-target">
+              <label for="new-address">新しいお届け先</label><br>
+              <label for="">郵便番号(ハイフンなし)</label>
+              <input type="text" placeholder="0001111"><br>
+              <label for="">住所</label>
+              <input type="text" placeholder="東京都豊島区池袋0-0-0"><br>
+              <label for="">宛名</label>
+              <input type="text" placeholder="藤浪翔平"><br>
+            </li>
+          </ul>
+          <input type="submit" name="input_order" class="btn btn-outline-primary btn-lg" value="確認画面へ">
+        </div>
+      </form>
+    </div>
   </div>
 </div>
 
