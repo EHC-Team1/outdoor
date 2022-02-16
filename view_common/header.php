8b2a86de44458074fd3382eae98a16d00164b2be
EguchiAkira0908 <Akira.Eguchi2@kusurinomadoguchi.co.jp>
2022-02-16 14:10:58 +0900

    ヘッダー作成

--- view_common/header.php
+++ view_common/header.php
@@ -17,4 +17,36 @@
 
 <body>
   <header>
+    <!-- ログイン状態か判別 -->
+    <?php if (isset($_SESSION['customer'])) { ?>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="../view_public/top.php">OUTDOOR</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <form method="post" class="d-flex">
+              <button type="submit" name="logout" class="btn btn-outline-secondary">ログアウト</button>
+            </form>
+            <button onclick="location.href='cart_item_index.php'" class="btn btn-outline-info">カートを見る</button>
+          </div>
+        </div>
+      </nav>
+    <?php } else { ?>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="../view_public/top.php">OUTDOOR</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-target="#header_item" aria-controls="header_item" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="header_item">
+            <ul class="navbar-nav">
+              <button onclick="location.href='public_login.php'" class="btn btn-outline-primary">ログイン</button>
+              <button onclick="location.href='public_signup.php'" class="btn btn-outline-info">カートを見る</button>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <?php } ?>
   </header>
--- view_public/top.php
+++ view_public/top.php
@@ -9,52 +9,95 @@ $articles = $pdo->index();
 $articles = $articles->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<?php require_once '../view_common/header.php'; ?>
+<?php
+// require_once '../view_common/header.php';
+?>
 
-<div class="main-visual">
-  <div class="slide1"></div>
-  <div class="slide2"></div>
-  <div class="slide3"></div>
-  <div class="slide4"></div>
-  <div class="slide5"></div>
-</div>
+<!DOCTYPE html>
+<html lang="ja">
 
-<div class="container">
-  <div class="row d-flex align-items-center justify-content-center">
-    <div class="col-md-4">
-      <?php require_once '../view_public/sidebar.php'; ?>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/bootstrap.css">
+  <!-- BootstrapのJS読み込み -->
+  <script src="../js/bootstrap.min.js"></script>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src='https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.5.0.js'></script>
+  <!-- スライド用slick -->
+  <link rel="stylesheet" href="../css/slick-theme.css" type="text/css">
+  <link rel="stylesheet" href="../css/slick.css" type="text/css">
+  <script type="text/javascript" src="http://code.jquery.com/jquery-3.1.0.min.js"></script>
+  <script src="../js/slick.js" type="text/javascript"></script>
+  <title>Outdoor</title>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="../view_public/top.php">OUTDOOR</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <button onclick="location.href='public_login.php'" class="btn btn-outline-primary nav-link">ログイン</button>
+          </li>
+          <li class="nav-item active">
+            <button onclick="location.href='public_signup.php'" class="btn btn-outline-info nav-link">カートを見る</button>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
 
-    <div class="col-md-8">
-      <table class="table">
-        <tbody>
-          <?php foreach ($articles as $article) {
-            $target = $article["article_image"]; ?>
-            <tr>
-              <td rowspan="3">
-                <?php if ($article["extension"] == "jpeg" || $article["extension"] == "png" || $article["extension"] == "gif") { ?>
-                  <img src="../view_common/article_image.php?target=<?= $target ?>" alt="article_image" width=200 height=200>
-                <?php } ?>
-              </td>
-              <td>
-                <h5><?= $article['title'] ?></h5>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p><?= $article['body'] ?></p>
-              </td>
-            </tr>
-            <tr>
-              <p><small class="text-muted"><?= $article['updated_at'] ?></small></p>
-            </tr>
-          <?php } ?>
-        </tbody>
-      </table>
+  <div class="main-visual">
+    <div class="slide1"></div>
+    <div class="slide2"></div>
+    <div class="slide3"></div>
+    <div class="slide4"></div>
+    <div class="slide5"></div>
+  </div>
+
+  <div class="container">
+    <div class="row d-flex align-items-center justify-content-center">
+      <div class="col-md-4">
+        <?php require_once '../view_public/sidebar.php'; ?>
+      </div>
+
+      <div class="col-md-8">
+        <table class="table">
+          <tbody>
+            <?php foreach ($articles as $article) {
+              $target = $article["article_image"]; ?>
+              <tr>
+                <td rowspan="3">
+                  <?php if ($article["extension"] == "jpeg" || $article["extension"] == "png" || $article["extension"] == "gif") { ?>
+                    <img src="../view_common/article_image.php?target=<?= $target ?>" alt="article_image" width=200 height=200>
+                  <?php } ?>
+                </td>
+                <td>
+                  <h5><?= $article['title'] ?></h5>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p><?= $article['body'] ?></p>
+                </td>
+              </tr>
+              <tr>
+                <p><small class="text-muted"><?= $article['updated_at'] ?></small></p>
+              </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
-</div>
 
-<!-- スライド用jsファイル -->
-<script src="../js/top.js"></script>
-<?php require_once '../view_common/footer.php'; ?>
+  <!-- スライド用jsファイル -->
+  <script src="../js/top.js"></script>
+  <?php require_once '../view_common/footer.php'; ?>
