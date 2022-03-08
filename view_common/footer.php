9410320e07a168d62f2d32d05c9353e0871fd003
EguchiAkira0908 <Akira.Eguchi2@kusurinomadoguchi.co.jp>
2022-03-08 19:02:20 +0900

    フッター修正

--- view_common/footer.php
+++ view_common/footer.php
@@ -1,23 +1,19 @@
 </main>
 
 <footer>
-  <div class="row d-flex justify-content-center">
-    <div class="col-sm-2 text-center">
-      <a href="../view_public/top.php" style="text-decoration:none">
-        <h5 style="color:black; line-height: 40px;">TOP</h5>
-      </a>
-    </div>
-    <div class="col-sm-2 text-center">
-      <a href="../view_public/about.php" style="text-decoration:none">
-        <h5 style="color:black; line-height: 40px;">ABOUT</h5>
-      </a>
-    </div>
-    <div class="col-sm-2 text-center">
-      <a href="../view_public/public_item_index.php" style="text-decoration:none">
-        <h5 style="color:black; line-height: 40px;">商品一覧</h5>
-      </a>
-    </div>
-  </div>
+
+  <a href="../view_public/top.php" style="text-decoration:none">
+    <span style="color:black; line-height: 40px;">TOP　</span>
+  </a>
+
+  <a href="../view_public/about.php" style="text-decoration:none">
+    <span style="color:black; line-height: 40px;">　|　　ABOUT　　|　</span>
+  </a>
+
+  <a href="../view_public/public_item_index.php" style="text-decoration:none">
+    <span style="color:black; line-height: 40px;">　商品一覧</span>
+  </a>
+
 </footer>
 
 <!-- スライド用jsファイル -->
